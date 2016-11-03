be565e7957616071c43c16717588e4c19161e18c
sbecirovic <seila.becirovic@gmail.com>
2016-11-03 19:05:23 +0100

    Delete meni.js

--- Spirala1/meni.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementByID("mojanav");
-    if (x.className === "navigacija") {
-        x.className += ".responsive";
-    } else {
-        x.className = "navigacija";
-    }
-}
