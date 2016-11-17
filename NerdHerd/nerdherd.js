450c39d1dbe924a4ac034f4fb02a7daab4bb9e43
sbecirovic <seila.becirovic@gmail.com>
2016-11-17 22:20:17 +0100

    All I ever wanted, all I ever needed is here in ajax
    Signed-off-by: sbecirovic <seila.becirovic@gmail.com>

--- NerdHerd/nerdherd.js
+++ NerdHerd/nerdherd.js
@@ -85,3 +85,125 @@ function prikazCarousel(n) {
   dots[slideIndex-1].className += " active";
 
 }
+
+function unosRev(){
+  var tacnost=false;
+  var greska = document.getElementsByClassName('greska')[0];
+  var ime=document.getElementsByName('name')[0];
+  var title=document.getElementsByName('title')[0];
+  var tekst=document.getElementsByName('Tekst')[0];
+  var email=document.getElementsByName('email')[0];
+  greska.innerHTML= "";
+  var imeRegEx=/^[a-zA-Z ]{2,30}$/;
+  var emailRegEx=/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+  if(ime.value.length<1 || !imeRegEx.test(ime.value))
+  {
+    ime.style.borderColor="#421C5";
+    greska.innerHTML+="<li>Name is not valid</li>";
+    tacnost=false;
+  }
+  else{
+    ime.style.borderColor="#b793b7";
+    tacnost=true;
+}
+
+if(email.value.length<0 || !emailRegEx.test(email.value))
+    {
+      email.style.borderColor="#421C5";
+      greska.innerHTML+="<li>Email is not valid</li>";
+      tacnost=false;
+    }
+    else{
+      email.style.borderColor="#b793b7";
+      tacnost=true;
+  }
+if(title.value.length<1 || !imeRegEx.test(title.value))
+{
+  title.style.borderColor="#421C5";
+  greska.innerHTML+="<li>Title is not valid</li>";
+  tacnost=false;
+}
+else{
+  title.style.borderColor="#b793b7";
+  tacnost=true;
+}
+if(tekst.value.length<500)
+{
+tekst.style.borderColor="#421C5";
+greska.innerHTML+="<li>Review is not valid</li>";
+tacnost=false;
+}
+else{
+tekst.style.borderColor="#b793b7";
+tacnost=true;
+}
+if(!tacnost) greska.style.display="inline-block";
+return tacnost;
+}
+
+function unosKom(){
+  var tacnost=false;
+  var greska = document.getElementsByClassName('greska')[0];
+  var ime=document.getElementsByName('name')[0];
+  greska.innerHTML= "";
+  var imeRegEx=/^[a-zA-Z ]{2,30}$/;
+
+  if(ime.value.length<1 || !imeRegEx.test(ime.value))
+  {
+    ime.style.borderColor="#421C5";
+    greska.innerHTML+="<li>Name is not valid</li>";
+    tacnost=false;
+  }
+  else{
+    ime.style.borderColor="#b793b7";
+    tacnost=true;
+}
+
+if(!tacnost) greska.style.display="inline-block";
+return tacnost;
+}
+
+function unosKont(){
+  var tacnost=false;
+  var greska = document.getElementsByClassName('greska')[0];
+  var ime=document.getElementsByName('name')[0];
+  var tekst=document.getElementsByName('Tekst')[0];
+  var email=document.getElementsByName('email')[0];
+  greska.innerHTML= "";
+  var imeRegEx=/^[a-zA-Z ]{2,30}$/;
+  var emailRegEx=/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+  if(ime.value.length<1 || !imeRegEx.test(ime.value))
+  {
+    ime.style.borderColor="#421C5";
+    greska.innerHTML+="<li>Name is not valid</li>";
+    tacnost=false;
+  }
+  else{
+    ime.style.borderColor="#b793b7";
+    tacnost=true;
+}
+
+if(email.value.length<0 || !emailRegEx.test(email.value))
+    {
+      email.style.borderColor="#421C5";
+      greska.innerHTML+="<li>Email is not valid</li>";
+      tacnost=false;
+    }
+    else{
+      email.style.borderColor="#b793b7";
+      tacnost=true;
+  }
+
+if(tekst.value.length<50)
+{
+tekst.style.borderColor="#421C5";
+greska.innerHTML+="<li>Contact message is not valid</li>";
+tacnost=false;
+}
+else{
+tekst.style.borderColor="#b793b7";
+tacnost=true;
+}
+if(!tacnost) greska.style.display="inline-block";
+return tacnost;
+}
